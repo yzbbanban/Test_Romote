67e88bb25073b865879670fa1d887191f694f72b
yzbbanban <yzbbanban@live.com>
2017-03-24 18:44:48 +0800

    添加定时任务

--- app/src/main/java/com/bb/yzbbanban/test_romote/service/Service1.java
+++ app/src/main/java/com/bb/yzbbanban/test_romote/service/Service1.java
@@ -2,25 +2,51 @@ package com.bb.yzbbanban.test_romote.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.icu.text.DateIntervalInfo;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.util.*;
+
 /**
  * This Service is Persistent Service. Do some what you want to do here.<br/>
- *
+ * <p>
  * Created by Mars on 12/24/15.
  */
-public class Service1 extends Service{
+public class Service1 extends Service {
     private static final String TAG = "Service1";
+    private Timer timer;
+    private TimerTask timerTask;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 1:
+                    Toast.makeText(Service1.this, "ban", Toast.LENGTH_SHORT).show();
+                    Log.i(TAG, "handleMessage: ban"+new Date());
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
+
     @Override
     public void onCreate() {
         super.onCreate();
         //TODO do some thing what you want..
-        Toast.makeText(this, "ban", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "ban", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "ban", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "ban", Toast.LENGTH_SHORT).show();
+
+        timer = new Timer();
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                mHandler.sendEmptyMessage(1);
+            }
+        };
+        timer.schedule(timerTask, 3000, 3000);
+
 
     }
 
